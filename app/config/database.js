3d09bd7bc3557f21b700439c4829cbc5e23d32c0
elfinsanjaya12 <elfinsanjaya1207@gmail.com>
2021-11-16 14:30:05 +0700

    setting posgres and deploy with heroku

--- app/config/database.js
+++ app/config/database.js
@@ -14,10 +14,15 @@ module.exports = {
     dialect: 'mysql',
   },
   production: {
-    username: 'root',
-    password: null,
-    database: 'database_production',
-    host: '127.0.0.1',
-    dialect: 'mysql',
+    username: 'aabplqahhuiclx',
+    password:
+      'c8ac7b987326263811ef8727396291574ea8667ba3126edba617f5b377f102c3',
+    database: 'd7qliesevp73nv',
+    host: 'ec2-34-198-189-252.compute-1.amazonaws.com',
+    post: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: { rejectUnauthorized: false },
+    },
   },
 };
